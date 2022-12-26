15499b5d706c17c1bab41a0d531744b9261c29ca
phant <roma0206@yandex.ru>
2022-12-26 16:58:56 +0300

    Избыток веса

--- src/Main.java
+++ src/Main.java
@@ -24,5 +24,24 @@ public class Main {
     System.out.println(friend);
     friend = friend /7;
     System.out.println(friend);
+    var frog = 3.5;
+    System.out.println(frog);
+    frog = frog * 10;
+    System.out.println(frog);
+    frog = frog/3.5;
+    System.out.println(frog);
+    frog = frog + 4;
+    System.out.println(frog);
+
+    var firstBoxer = 78.2;
+    var secondBoxer = 82.7;
+    var totalWeight = firstBoxer + secondBoxer;
+    System.out.println("Общий вес " + totalWeight + " кг");
+
+    var differentWeight = secondBoxer - firstBoxer;
+    System.out.println("Разница в весе " + differentWeight);
+
+    var surplusWeight = secondBoxer%firstBoxer;
+    System.out.println("Избыток "+ surplusWeight);
     }
 }
